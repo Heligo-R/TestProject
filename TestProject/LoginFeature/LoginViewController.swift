70831e7313f0ba77de2493705b95f10c70fe05d2
Heligo <o.repsh@mail.ru>
2020-06-19 00:50:05 +0300

    Login database update

--- TestProject/LoginFeature/LoginViewController.swift
+++ TestProject/LoginFeature/LoginViewController.swift
@@ -11,15 +11,46 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: ValidatableTextField!
     @IBOutlet weak var passwordTextField: ValidatableTextField!
     @IBOutlet weak var loginStackFields: UIStackView!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    let validation = ValidationManager()
+    let localRepo = LocalRepo()
+    
+    typealias Credentials = (String, String)
     
     @IBAction func textFieldDoneEditing(sender: UITextField) {
-        let validation = ValidationManager()
-        
         if sender == emailTextField {
             emailTextField.updateStatus(validation.verifyText(emailTextField.text, expression: .email))
         }
         if sender == passwordTextField {
             passwordTextField.updateStatus(validation.verifyText(passwordTextField.text, expression: .password))
         }
+        
+        errorLabel.isHidden = true
+    }
+    
+    @IBAction func touchButton(_ sender: UIButton) {
+        if sender == submitButton {
+            guard let credentials = readFields() else { return }
+            if localRepo.getUser(credentials) == nil { errorLabel.isHidden = false }
+            }
+        }
+    
+    func readFields() -> Credentials? {
+        var readingFailed = false
+        
+        func validateField (field: ValidatableTextField, expression: VerificationRegex){
+            let validationResult = validation.verifyText(field.text, expression: expression)
+            if validationResult != .filled { readingFailed = true }
+            field.updateStatus(validationResult)
+        }
+        
+        validateField(field: emailTextField, expression: .email)
+        validateField(field: passwordTextField, expression: .password)
+        
+        if readingFailed {
+            return nil
+        } else { return (emailTextField.text!, passwordTextField.text!) }
     }
 }
