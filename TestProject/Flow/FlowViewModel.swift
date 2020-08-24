8c6fb2db24219787c5feba44256ae212eeca31b9
Heligo <o.repsh@mail.ru>
2020-08-24 19:52:27 +0300

    Stash fix

---
+++ TestProject/Flow/FlowViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  FlowViewModel.swift
+//  TestProject
+//
+//  Created by Oleg on 17.08.2020.
+//  Copyright © 2020 Oleg. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+final class FlowViewModel {
+    var expandedCellIndex: IndexPath?
+    var news: [News]?
+    
+    func retrieveData() -> Observable<[News]?> {
+        let observable: Observable<Newsletter> = NewsDataManagerMock().proceedResponse()
+        return observable.map{ result in
+            result.news
+        }
+    }
+}
+
+
